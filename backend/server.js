0afc38f465453ae4759ef0547315a4cd88aa9ff7
Santos2175 <santoshgurung949@gmail.com>
2025-05-20 05:03:12 +0545

    chore(backend): backend setup

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+// PORT initialization
+const PORT = process.env.PORT || 5000;
+
+// App initialization
+const app = express();
+
+// middlewares
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Listening to server
+app.listen(PORT, () => {
+  console.log(`Server running on PORT: ${PORT}`);
+});
